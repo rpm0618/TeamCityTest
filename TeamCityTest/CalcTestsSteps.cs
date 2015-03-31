232ac60ea3bf9582fd7426ae6a816f8eba2a1e40
Ryan McVeety <rpm0618@gmail.com>
2015-03-31 11:42:53 -0400

    Initial Commit

---
+++ TeamCityTest/CalcTestsSteps.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using NUnit.Framework;
+
+namespace TeamCityTest
+{
+    [Binding]
+    public class CalcTestsSteps
+    {
+        private Calculator calc;
+
+        public CalcTestsSteps()
+        {
+            calc = new Calculator();
+        }
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int number)
+        {
+            calc.EnterNumber(number);
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            calc.Add();
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int number)
+        {
+            Assert.That(calc.GetScreen(), Is.EqualTo(number));
+        }
+    }
+}
---
+++ TeamCityTest/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamCityTest
+{
+    class Calculator
+    {
+        private List<int> numbers;
+
+        private int screen;
+
+        public Calculator()
+        {
+            numbers = new List<int>();
+        }
+
+        public void EnterNumber(int number)
+        {
+            numbers.Add(number);
+        }
+
+        public void Add()
+        {
+            screen = numbers.Aggregate((x, y) => x + y);
+        }
+
+        public int GetScreen()
+        {
+            return screen;
+        }
+    }
+}
